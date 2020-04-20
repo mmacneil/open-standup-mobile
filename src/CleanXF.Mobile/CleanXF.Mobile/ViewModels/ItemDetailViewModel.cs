91555b05f1ebb9969e4231e716af45deaab18d29
mmacneil <markmacneil@gmail.com>
2020-04-19 21:19:13 -0300

    Import.

---
+++ src/CleanXF.Mobile/CleanXF.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CleanXF.Mobile.Models;
+
+namespace CleanXF.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
