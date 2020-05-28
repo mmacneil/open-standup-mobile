3996a4cae84a9def7d2bf82330bbe29ad3f39fb2
mmacneil <markmacneil@gmail.com>
2020-05-27 22:44:59 -0300

    Mediatr + DI

--- src/CleanXF.Core/CoreModule.cs
+++ src/CleanXF.Core/CoreModule.cs
@@ -22,10 +22,6 @@ namespace CleanXF.Core
                 return t => c.Resolve(t);
             });
 
-            // finally register our custom code (individually, or via assembly scanning)
-            // - requests & handlers as transient, i.e. InstancePerDependency()
-            // - pre/post-processors as scoped/per-request, i.e. InstancePerLifetimeScope()
-            // - behaviors as transient, i.e. InstancePerDependency()
             builder.RegisterAssemblyTypes(typeof(LoginUseCase).GetTypeInfo().Assembly).AsImplementedInterfaces(); // via assembly scan
         }
     }
