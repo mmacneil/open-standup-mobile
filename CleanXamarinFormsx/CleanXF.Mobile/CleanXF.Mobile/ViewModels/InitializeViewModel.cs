76e39274d5751ec164c22207cd99c74b124faff2
mmacneil <markmacneil@gmail.com>
2020-08-07 22:02:58 -0300

    Wip.

--- CleanXamarinFormsx/CleanXF.Mobile/CleanXF.Mobile/ViewModels/InitializeViewModel.cs
+++ CleanXamarinFormsx/CleanXF.Mobile/CleanXF.Mobile/ViewModels/InitializeViewModel.cs
@@ -1,31 +1,30 @@
-﻿using CleanXF.Mobile.Services;
+﻿using CleanXF.Core.Interfaces.Data.Repositories;
+using CleanXF.Mobile.Services;
 using ShellLogin.ViewModels;
 using System.Threading.Tasks;
 
 public class InitializeViewModel : BaseViewModel
 {
-   // private readonly ISessionRepository _sessionRepository;
-   private readonly INavigator _navigator;
-    public InitializeViewModel(/*ISessionRepository sessionRepository, */ INavigator navigator)
+    private readonly ISessionRepository _sessionRepository;
+    private readonly INavigator _navigator;
+    public InitializeViewModel(ISessionRepository sessionRepository, INavigator navigator)
     {
-       // _sessionRepository = sessionRepository;
-       _navigator = navigator;
+        _sessionRepository = sessionRepository;
+        _navigator = navigator;
     }
 
     public async void Initialize()
     {
-        await Task.Delay(1337);
-
-        await _navigator.GoTo("///login");
+        await Task.Delay(1300);
 
         // If we have an access token we're considered logged in so proceed to shell, otherwise route to login
-        /* if (await _sessionRepository.HasAccessToken())
-         {
-             await _navigator.GoTo("///main");
-         }
-         else
-         {
-             await _navigator.GoTo("///login");
-         }*/
+        if (await _sessionRepository.HasAccessToken())
+        {
+            await _navigator.GoTo("///main");
+        }
+        else
+        {
+            await _navigator.GoTo("///login");
+        }
     }
 }
