e898e64a5cd0a7f0788d04237c2cf6bfe933d4f1
mmacneil <markmacneil@gmail.com>
2021-02-15 22:22:58 -0400

    Prep for publishing.

--- src/OpenStandup/OpenStandup.Mobile.Infrastructure/Configuration/AppSettings.cs
+++ src/OpenStandup/OpenStandup.Mobile.Infrastructure/Configuration/AppSettings.cs
@@ -19,7 +19,7 @@ namespace OpenStandup.Mobile.Infrastructure.Configuration
             get
             {
 #if DEBUG
-                return https://192.168.0.141:45455";
+                return "https://192.168.0.141:45455";
 #else
                 return "https://openstandup.com";
 #endif
